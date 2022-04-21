70af55fe00a3891e3af190e9287b219ceea28113
Ajay Sonere <89720260+ajaysonere@users.noreply.github.com>
2022-04-21 17:35:58 +0530

    Sum of N number in array

---
+++ sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int sum_of_array(int arr[],int n)
+{
+   if(n==0)
+      return 0;
+   return arr[0]+sum_of_array(arr+1,n-1);
+}
+int main()
+{
+   int n;
+   cin >>n;
+   int arr[n];
+   for(int i=0;i<n;i++)
+   {
+     cin >>arr[i];
+   }
+   cout << sum_of_array(arr,n);
+   return 0;
+}
