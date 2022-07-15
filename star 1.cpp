f46a25ff1078ed64caf6b5dcc7b3458f87f3dbbd
Ajay Sonere <89720260+ajaysonere@users.noreply.github.com>
2022-07-15 21:58:22 +0530

    print star

---
+++ star 1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+  int i,j;
+  for (i=1;i<=5;i++)
+  {
+      for(j=1;j<=i;j++)
+      {
+        //  if (j<=i)
+        //  {
+        //      cout << "*";
+        //  } 
+        //  else{
+        //      cout << " ";
+        //  }
+        cout << "*";
+      }
+      cout << endl;
+  }
+return 0;
+}
