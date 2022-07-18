905697f703bf1059450b34c39f4ad02010c5424d
Ajay Sonere <89720260+ajaysonere@users.noreply.github.com>
2022-07-18 23:24:22 +0530

    And operator

---
+++ andoperater.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+  int n;
+  cin >> n;
+  if(n&1==1)
+  {
+      cout << "N is Odd :";
+  }
+  else
+  {
+      cout <<"N is Even :";
+  }
+return 0;
+}
